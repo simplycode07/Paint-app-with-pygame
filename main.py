1311b7edb24204182b245ca4dc1908186d1f84a1
simplycode07 <simplycode07@gmail.com>
2022-09-11 16:29:58 +0530

    Git initialise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame.locals import *
 import random
+import time
+
 from datetime import datetime
 
 pygame.init()
@@ -10,8 +12,9 @@ display_size = (1000,650)
 display = pygame.display.set_mode(display_size)
 pygame.display.set_caption("Painting App with pygame")
 clock = pygame.time.Clock()
-font = pygame.font.Font("C:\Windows\Fonts\Arial.ttf", 18)
+font = pygame.font.Font("C:\\Windows\\Fonts\\Arial.ttf", 18)
 
+square_selected = False
 # colors
 white = (255,255,255)
 black = (0,0,0)
@@ -39,22 +42,33 @@ def save_image():
     return screenshot
 
 def main():
+    square_selected = False
+    click_num = 0
     size = 25
     pen_color = (255,255,0)
     eraser_color = white
     color_button = []
+    rect_pos = []
     display.blit(text, text_rect)
     for i in range(len(colors)):
         color_button.append(pygame.Rect((5+15*i), 10, 10,10))
         pygame.draw.rect(display, colors[i], pygame.Rect((5+15*i), 10, 10,10))
+    
+    square_rect = pygame.Rect(5,35, 10,10)
+    pygame.draw.rect(display, (200,200,200), square_rect)
     while True:
         for event in pygame.event.get():
             pos = pygame.mouse.get_pos()
             keys = pygame.key.get_pressed()
             if event.type == pygame.QUIT:
                 print("closing")
-                pygame.quit()
-                break
+                quit()
+                
+            if square_selected:
+                if pygame.mouse.get_pressed() == (1,0,0) and click_num != 2:
+                    click_num += 1
+                    rect_pos = rect_pos.append(pos)
+                    
             if keys[pygame.K_KP_PLUS] and size < 200:
                 size+=1
             if keys[pygame.K_KP_MINUS] and size != 0:
@@ -65,11 +79,32 @@ def main():
                 pygame.image.save(image, f"{date.lower()}-image.jpg")
 
             if pygame.mouse.get_pressed() == (1,0,0):
-                 for i in range(len(colors)):
-                    if color_button[i].collidepoint(pos):
-                        color = colors[i]
+                #  for i in range(len(colors)):
+                #     if color_button[i].collidepoint(pos):
+                #         color = colors[i]
                 if text_rect.collidepoint(pos):
-                    pygame.draw.rect(display, white, pygame.Rect(0,20,display_size[0],display_size[1]))
+                	pygame.draw.rect(display, white, pygame.Rect(0,50,display_size[0],display_size[1]))
+                if not square_selected:
+                    if color_button[0].collidepoint(pos):
+                        pen_color = colors[0]
+                    if color_button[1].collidepoint(pos):
+                        pen_color = colors[1]
+                    if color_button[2].collidepoint(pos):
+                        pen_color = colors[2]
+                    if color_button[3].collidepoint(pos):
+                        pen_color = colors[3]
+                    if color_button[4].collidepoint(pos):
+                        pen_color = colors[4]
+                    if color_button[5].collidepoint(pos):
+                        pen_color = colors[5]
+                    if color_button[6].collidepoint(pos):
+                        pen_color = colors[6]
+                    if color_button[7].collidepoint(pos):
+                        pen_color = colors[7]
+                if square_rect.collidepoint(pos):
+                    print("square pressed")
+                    square_selected = True
+                    time.sleep(1)
 
                 if (pos[1]-size//2) > 20 and (pos[0]-size//2) > 50:
                     pygame.draw.rect(display, pen_color, pygame.Rect((pos[0]-size//2), (pos[1]-size//2), size, size))
@@ -82,5 +117,4 @@ def main():
         clock.tick(240)
 
 main()
-pygame.quit()
 quit()
