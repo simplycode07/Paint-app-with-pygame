a28a27167728219347e2be39209d5b7c2480a74d
Dhruv Dubey <simplycode07@gmail.com>
2024-06-26 23:46:32 +0530

    continuous drawing with weird width

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import pygame
-from src import settings, tools
+from src import settings, tools, colors
 
 pygame.init()
 
 display = pygame.display.set_mode(settings.resolution)
 pygame.display.set_caption("brr")
-pen = tools.Pen((0, 0, 0), 15)
+pen = tools.Pen(colors.black, 15)
 
 running = True
 
-display.fill((255, 255, 255))
+display.fill(colors.white)
 
 while running:
     for event in pygame.event.get():
@@ -18,10 +18,13 @@ while running:
 
         if pygame.mouse.get_pressed() == (1, 0, 0):
             pos = list(pygame.mouse.get_pos())
-            pos[1] -= settings.ui_height
-            canvas = pen.draw(pos)
-            display.blit(canvas, (0, settings.ui_height))
+            if pos[1] > settings.ui_height:
+                # because the surface starts from (0, ui_height) and the mouse input is with respect to origin of display 
+                pos[1] -= settings.ui_height
+                pen.add(pos)
 
+    canvas = pen.refresh()
+    display.blit(canvas, (0, settings.ui_height))
     pygame.display.flip()
 
 pygame.quit()
--- src/tools.py
+++ src/tools.py
@@ -1,15 +1,30 @@
 import pygame
-from . import settings
+from . import settings, colors
 
-pygame.init()
+
+canvas = pygame.Surface((settings.resolution[0], settings.resolution[1] - settings.ui_height))
+canvas.fill(colors.white)
 
 class Pen:
     def __init__(self, color, size) -> None:
         self.color = color
         self.size = size
-        self.canvas = pygame.Surface((settings.resolution[0], settings.resolution[1] - settings.ui_height))
-        self.canvas.fill((255, 255, 255))
+        self.positions = []
 
-    def draw(self, pos):
-        pygame.draw.rect(self.canvas, self.color, pygame.Rect((pos[0]-self.size//2), (pos[1]-self.size//2), self.size, self.size))
-        return self.canvas
+    def add(self, pos):
+        self.positions.append(pos)
+        print(self.positions)
+
+    def refresh(self):
+        while len(self.positions) > 1:
+            pygame.draw.line(canvas, self.color, self.positions[0], self.positions[1], self.size)
+            self.positions.pop(0)
+
+        if len(self.positions):
+            rect_x = self.positions[0][0] - self.size//2
+            rect_y = self.positions[0][1] - self.size//2
+
+            pygame.draw.rect(canvas, self.color, pygame.Rect(rect_x, rect_y, self.size, self.size))
+        
+        return canvas
+
